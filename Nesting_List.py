a7ae0b335f92f74ff9c2fe1a5b05c9b0b9c82fad
ayushathare <ayushathare18@gmail.com>
2024-03-15 11:24:49 +0530

    Initial

---
+++ Nesting_List.py
@@ -0,0 +1,4 @@
+#Nesting of List
+
+A = [[1,2,3],[2,4,3],[6,5,4]]
+print(A)
--- list.py
+++ list.py
@@ -1,2 +1,7 @@
 my_list = [1, 2, 3, 4, 5]
 print(my_list)
+
+## Inserting Nesting of list 
+
+A = [[1,2,3],[3,2,1],[4,4,4],[8,6,5]]
+print(A)
