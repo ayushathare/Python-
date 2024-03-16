c9987cd3d38fc52d32d14e8d4480dd86d089e7a8
ayushathare <ayushathare18@gmail.com>
2024-03-16 11:49:57 +0530

    Initial Code

--- Nesting_List.py
+++ Nesting_List.py
@@ -1,10 +1,17 @@
 #Nesting of List
 
-A = [[1,2,3],[2,4,3],[6,5,4]]
+A = [[1,2,3],[2,4,3],[6,5,4],[45,"Aman"]]
 print(A)
 
 # Accessing a perticular list element
 
-print(A[1])
+print (A[1])
 
-print(A[1][1])
+print (A[1][1])
+print (A[3][1])
+
+## Creating New List
+
+amazon_cart = [["Watch",5000],["Phone",10000],["Laptop",50000]]
+print(amazon_cart)
+
