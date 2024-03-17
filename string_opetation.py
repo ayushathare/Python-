03abbcac277c4e74e5adfa020ae3221afa07f330
ayushathare <ayushathare18@gmail.com>
2024-03-17 13:57:16 +0530

    Initial Code

--- string_opetation.py
+++ string_opetation.py
@@ -1,2 +1,24 @@
+string = "Hello World"
+
+# String Opearation
+
+print(len(string))
+print(string.capitalize())
+print(string.upper())
+print(string.lower())
+print(string.strip())
+
+
+# Replacing the string Element
+new_string = string.replace("World","Universe")
+
+string1 = "Apple,Banana,Orange"
+fruits = string1.split(",")
+print(fruits)
+
+
+
+
+
 
 
