7ed0c8635c9a3bff1e1e98e9fe961f6e458463e7
ayushathare <ayushathare18@gmail.com>
2024-03-17 14:08:56 +0530

    Initial Code

---
+++ input_fun.py
@@ -0,0 +1,8 @@
+string = input("Enter the Stirng")
+print("You Enter a string",string)
+
+number = int(input("Enter the number"))
+print("You Enter Integer ",number)
+
+number_float = float(input("Enter a Float value"))
+print("you entered value is ",number_float)
