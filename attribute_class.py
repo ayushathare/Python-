f1250c594448e4c5c794759afb1b02e10de76d32
ayushathare <ayushathare18@gmail.com>
2024-03-21 11:19:13 +0530

    Initial Code

---
+++ attribute_class.py
@@ -0,0 +1,16 @@
+class Anumal:
+    sp_name = ""
+    common_name = ""
+    age = 0
+
+def __init__(self,name,type1,age1):
+    self.sp_name = name
+    self.common_name = type1
+    self.age = age1
+
+def fun(Self):
+    print("My name is",self.sp_name)
+    print("My type is ",self.common_name)
+    print("My age is",self.age)
+
+def edit
---
+++ swaping.py
@@ -0,0 +1,13 @@
+#Swaping 2 numbers
+a = int(input("Enter First Number"))
+b = int(input("Enter Third Number"))
+
+print("Original Value is")
+print("a",a)
+print("b",b)
+
+a,b=b,a
+
+print("After Swaping numbers")
+print(a)
+print(b)
